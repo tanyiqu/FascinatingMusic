7e25b1ca381424b1c50bebf73c351810787f3fa0
Tanyiqu <1953649096@qq.com>
2020-05-13 11:24:35 +0800

    修复细节

--- FascinatingMusic-WeChat/data/dynamicData.js
+++ FascinatingMusic-WeChat/data/dynamicData.js
@@ -8,53 +8,53 @@ var dynamic = [{
   "url": "../play/play?musicId=0000042"
 }, {
   "did": "02",
-  "cover": "http://localhost:8080/resource/music/Farewell.jpg",
+  "cover": "http://localhost:8080/resource/music/Rolling In The Deep.jpg",
   "time": "4月23日",
   "content": "音乐真是个神奇的东西：即使已经过了一整年，再听这首歌，还是一下子把人拉回到那些晚上、就像高三夏天的凌晨一样难以忘怀",
-  "name": "Endless Tears",
-  "author": "CLIFF EDGE",
-  "url": "../play/play?musicId=0000042"
+  "name": "Rolling In The Deep.mp3",
+  "author": "Adele",
+  "url": "../play/play?musicId=0000003"
 }, {
   "did": "03",
-  "cover": "http://localhost:8080/resource/music/Farewell.jpg",
+  "cover": "http://localhost:8080/resource/music/Faded.jpg",
   "time": "4月17日",
   "content": "讨厌把虚荣心和无知表现出来又死不承认的天真又单纯的人，因为他会跟你争论，我得努力把话说的好听委婉点。我实在受不了那两副样子。",
-  "name": "Endless Tears",
-  "author": "CLIFF EDGE",
-  "url": "../play/play?musicId=0000042"
+  "name": "Faded",
+  "author": "Alan Walker",
+  "url": "../play/play?musicId=0000009"
 }, {
   "did": "04",
-  "cover": "http://localhost:8080/resource/music/Farewell.jpg",
+  "cover": "http://localhost:8080/resource/music/Alone.jpg",
   "time": "4月15日",
   "content": "我在烦什么呀。本来就是不会去再多想的事，怎么会那样呢？哎，不管了，彻底不管了。且烦着呗",
-  "name": "Endless Tears",
-  "author": "CLIFF EDGE",
-  "url": "../play/play?musicId=0000042"
-  }, {
-    "did": "05",
-    "cover": "http://localhost:8080/resource/music/Farewell.jpg",
-    "time": "4月13日",
-    "content": "啥时候能回去呢？\n过两天吧。\n明天会有任务给我么？\n真不想动啊。",
-    "name": "Endless Tears",
-    "author": "CLIFF EDGE",
-    "url": "../play/play?musicId=0000042"
-  }, {
-    "did": "06",
-    "cover": "http://localhost:8080/resource/music/Farewell.jpg",
-    "time": "4月06日",
-    "content": "我们也曾在班级打闹嬉戏我们也曾为了对方的成绩不好而烦恼原来拆散我们的并不是爱情的流逝是你我逝去的青春",
-    "name": "Endless Tears",
-    "author": "CLIFF EDGE",
-    "url": "../play/play?musicId=0000042"
-  }, {
-    "did": "07",
-    "cover": "http://localhost:8080/resource/music/Farewell.jpg",
-    "time": "3月31日",
-    "content": "如果太胖就积极运动，皮肤不好就改善饮食，五官改变不了就改变气质，不懂穿衣就去学，永远不要让自己停止美好。",
-    "name": "Endless Tears",
-    "author": "CLIFF EDGE",
-    "url": "../play/play?musicId=0000042"
-  }]
+  "name": "Alone",
+  "author": "Alan Walker Noonie Bao",
+  "url": "../play/play?musicId=0000011"
+}, {
+  "did": "05",
+  "cover": "http://localhost:8080/resource/music/Here With You.jpg",
+  "time": "4月13日",
+  "content": "啥时候能回去呢？\n过两天吧。\n明天会有任务给我么？\n真不想动啊。",
+  "name": "Here With You",
+  "author": "Asher Monroe",
+  "url": "../play/play?musicId=0000016"
+}, {
+  "did": "06",
+  "cover": "http://localhost:8080/resource/music/You.png",
+  "time": "4月06日",
+  "content": "我们也曾在班级打闹嬉戏我们也曾为了对方的成绩不好而烦恼原来拆散我们的并不是爱情的流逝是你我逝去的青春",
+  "name": "You",
+  "author": "Approaching Nirvana",
+  "url": "../play/play?musicId=0000015"
+}, {
+  "did": "07",
+  "cover": "http://localhost:8080/resource/music/Shape of You.jpg",
+  "time": "3月31日",
+  "content": "如果太胖就积极运动，皮肤不好就改善饮食，五官改变不了就改变气质，不懂穿衣就去学，永远不要让自己停止美好。",
+  "name": "Shape of You.mp3",
+  "author": "Ed Sheeran",
+  "url": "../play/play?musicId=0000064"
+}]
 
 
 module.exports = {
